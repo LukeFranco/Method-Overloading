61e61720cd1dbd3c6ebbbc15889eda96a33768e5
Luke Franco <94336719+LukeFranco@users.noreply.github.com>
2022-08-25 20:57:51 -0400

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,8 @@
+class Main {
+  public static void main(String[] args) {
+  Vehicle v = new Guitar();
+  System.out.println(v.car(4));
+System.out.println(v.vehicle1(4, " metal frame!"));
+    System.out.println(g.vehicle2(0," water splashing",6));
+  }
+}
