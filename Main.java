75492be97c91921497f30c4fae51ad9fd3edd94e
Luke Franco <94336719+LukeFranco@users.noreply.github.com>
2022-08-25 21:21:03 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ class Main {
   public static void main(String[] args) {
   Vehicle v = new Vehicle();
   System.out.println(v.car(4));
-System.out.println(v.vehicle1(4, " metal frame!"));
-    System.out.println(v.vehicle2(0)," water splashing",6));
+System.out.println(v.vehicle1(2, " wind sounds"));
+    System.out.println(v.vehicle2(0," water splashing",6));
   }
 }
