4dad1ef1e2d9d8bcc6e6b82403cdeeeb4d2cf68f
Luke Franco <94336719+LukeFranco@users.noreply.github.com>
2022-08-25 21:22:27 -0400

    Update Vehicle.java

--- Vehicle.java
+++ Vehicle.java
@@ -7,7 +7,7 @@
       return "I am a car with " + wheels + " wheels!";
 }
 public String vehicle1(int wheels, String sound){
-  return "The bicycle has " + wheels + " wheels and makes" + sound;
+  return "The ATV has " + wheels + " wheels and makes" + sound;
 }
   public String vehicle2(int strings , String sound, int size){
     return "Well my boat has " + wheels + " wheels, makes" + sound + ", and is about " + size + " feet!";
